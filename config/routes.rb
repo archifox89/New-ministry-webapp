1176c74eb40235454ee0bd4edd8b9c8f1d8e9f5c
Rashed Aljulaibi <rashed.jlb@gmail.com>
2015-05-31 12:17:35 +0300

    added sign in and log out funtion to local app

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   
+  devise_scope :user do
+  root :to => 'devise/sessions#new'
+end
   devise_for :users
-  root "pages#main"
+  get "main" => "pages#main"
   get "profile" => "pages#profile"
   get "projects" => "pages#projects"
   get "project_docs" => "pages#project_docs"
