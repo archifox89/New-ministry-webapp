2150eeb0db9e776c19d8c567e087f2b979984f4b
Rashed Aljulaibi <rashed.jlb@gmail.com>
2015-08-12 19:44:49 +0300

    more changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150811104215) do
+ActiveRecord::Schema.define(version: 20150812163540) do
 
   create_table "experiences", force: true do |t|
     t.string   "company"
